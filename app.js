3c10de2c2538ea449c727301765b50c51f05dfb4
ng1n3 <code.with.muyiwa@gmail.com>
2023-07-24 13:33:46 +0100

    finished game logic

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const displayController = (() => {
+	const renderMessage = (message) => {
+		document.querySelector('#message').innerHTML = message;
+	}
+
+	return {
+		renderMessage
+	}
+})();
+
 const Gameboard = (() => {
 	let gameboard = ["", "", "", "", "", "", "", "", ""];
 
@@ -19,7 +29,6 @@ const Gameboard = (() => {
 		gameboard[index] = value;
 		render();
 	};
-
 
 	// * accessroy method
 	const getGameboard = () => gameboard;
@@ -59,51 +68,63 @@ const Game = (() => {
 	};
 
 	const handleClick = (event) => {
+		if (gameOver) return;
 		let index = parseInt(event.target.id.split("-")[1]);
 		if (Gameboard.getGameboard()[index] !== "") return;
 
 		Gameboard.update(index, players[currentPlayerIndex].mark);
 
-		if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex.mark])) {
+		if (
+			checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex.mark])
+		) {
 			gameOver = true;
-			alert(`${players[currentPlayerIndex].name} won!`)
+			displayController.renderMessage(`${players[currentPlayerIndex].name} wins`)
 		} else if (checkForTie(Gameboard.getGameboard())) {
 			gameOver = true;
-			alert(`It's a tie!`);
+			displayController.renderMessage(`It's a Tie!`);
 		}
 
 		currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
 	};
 
 	const restart = () => {
-		for(let i = 0; i < 9; i++) {
-			Gameboard.update(i, '');
+		for (let i = 0; i < 9; i++) {
+			Gameboard.update(i, "");
 		}
 		Gameboard.render();
-	}
+	};
 
 	return {
 		start,
 		restart,
-		handleClick
+		handleClick,
 	};
 })();
 
 function checkForWin(board) {
-	const winningCombinations = [[0, 1, 2], [3, 4, 5], [6,7,8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-	winningCombinations.forEach((winning, i) => {
-		const[a, b, c] = winning[i];
-		if(board[a] && board[a] === board[b] && board[a] === board[c]) {
+	const winningCombinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < winningCombinations.length; i++) {
+		const [a, b, c] = winningCombinations[i];
+		if (board[a] && board[a] === board[b] && board[a] === board[c]) {
 			return true;
 		}
-	})
+	}
 
 	return false;
-
 }
 
 function checkForTie(board) {
-	return board.every(cell => cell !== '');
+	return board.every((cell) => cell !== "");
 }
 
 const restartBtn = document.querySelector("#restart-button");
